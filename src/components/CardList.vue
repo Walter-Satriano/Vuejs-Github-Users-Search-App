36620fe2f146fb24138287d5e335a24bb1ef3724
Walter <walter@quomi.it>
2021-12-07 12:30:47 +0100

    used the v-for directive on the Card tag to repeat it for every card we have received in our props

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,12 +1,27 @@
 <template>
-  <div></div>
+  <div>
+    <Card v-for="(card, index) in cards" :key="index"
+      :avatar_url=card.avatar_url
+      :name=card.name
+      :blog=card.blog
+      :location=card.location
+      :public_repos=card.public_repos
+      :html_url=card.html_url
+      :followers=card.followers
+    />
+  </div>
 </template>
 
 <script>
+import Card from './Card.vue'
+
 export default {
   name: 'CardList',
   props: {
     cards: Array,
   },
+  components: {
+    Card,
+  }
 }
 </script>
