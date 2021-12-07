7a82f58e353af365d94ddca7c5bdbfa8d11fb921
Walter <walter@quomi.it>
2021-12-07 12:29:53 +0100

    created card component that accepts props taken directly from the data coming from GitHub

---
+++ src/components/Card.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card-container">
+    <img v-bind:src="avatar_url" alt="user-avatar">
+    <div>
+      <div class="name">{{name}}</div>
+      <div>Location: {{location}}</div>
+      <div>Blog: {{blog}}</div>
+      <div>GitHub Url: {{html_url}}</div>
+      <div>Public Repository: {{public_repos}}</div>
+      <div>Followers: {{followers}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    blog: String,
+    name: String,
+    avatar_url: String,
+    location: String,
+    public_repos: Number,
+    html_url: String,
+    followers: Number
+  }
+}
+</script>
+
+<style scoped>
+.card-container {
+  margin: 20px;
+}
+.card-container img {
+  width: 70px;
+}
+.card-container .name {
+   font-weight: bold;
+}
+</style>
