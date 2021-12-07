7c8bd1829c2761feb759be5dcd15c2c8525c7cdd
Walter <walter@quomi.it>
2021-12-07 10:54:19 +0100

    created new Form Component
    The Form component holds an input element and a button. We enter a known/valid GitHub username in the input field, and when we press the button our application will go and ask GitHub for that user’s data.

---
+++ src/components/Form.vue
@@ -0,0 +1,27 @@
+<template>
+  <form @submit.prevent="addUser">
+    <input
+      type="text"
+      v-model="username"
+      placeholder="Github Username"
+      required
+    />
+    <button type="submit">Add Card</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "Form",
+  data: function() {
+    return {
+      username: '',
+    }
+  },
+  methods: {
+    addUser: function() {
+      alert(this.username)
+    }
+  }
+}
+</script>
