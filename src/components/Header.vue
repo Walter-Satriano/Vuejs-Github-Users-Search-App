55bdfc4f3abd984e725dfd00ca7c6a3bce0e2200
Walter <walter@quomi.it>
2021-12-11 12:58:29 +0100

    created header component

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="background">
+    <!-- <img class="img-fluid" src="@/assets/world-connection.jpg" alt=""> -->
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .background {
+    //width: 100%;
+    height: 1160px;
+    background-image: url("../assets/internet-connection.jpg");
+    background-size: cover;
+    background-position: bottom;
+  }
+
+</style>
